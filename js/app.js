9c14091c0e37d9839c0db4868cccb7d7da0b16c7
Kyle Geary <Kgeary09@gmail.com>
2018-09-05 22:33:30 -0500

    Add star and clock logic

--- js/app.js
+++ js/app.js
@@ -1,36 +1,55 @@
 
-//Declare card and move variables
-
+/** card and move variables **/
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+const stars = document.querySelectorAll(".fa-star");
 let matchedCard = document.getElementsByClassName("match");
 let openedCards = [];
-let moves = 0;
-const card = document.getElementsByClassName("card");
-let cards = [...card];
+let move = 0;
 const counter = document.querySelector(".moves");
 const deck = document.querySelector(".deck");
 const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".close-button");
 
 
-//Card event listener
+/** card and modal event listeners **/
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+modal.addEventListener("click", toggleModal);
 for (let i = 0; i < cards.length; i++){
 	cards[i].addEventListener("click", addToArray);
 	cards[i].addEventListener("click", displayCard);
 }
 
-//Display card function
+/** game clock **/
+let second = 0, minute = 0; hour = 0;
+let timer = document.querySelector(".clock");
+let interval;
+function startTimer() {
+	interval = setInterval(function () {
+		timer.innerHTML = minute + " mins " + second + " secs";
+		second++;
+		if (second == 60) {
+			minute++;
+			second = 0;
+		}
+		if (minute == 60) {
+			hour++;
+			minute = 0;
+		}
+	}, 1000);
+}
+
+/** Card display, array, and shuffle functions (shuffle from http://stackoverflow.com/a/2450976) **/
 function displayCard() {
 	this.classList.toggle("disabled");
 	this.classList.toggle("open");
 	this.classList.toggle("show");
 	cardOpen();
 }
-//Push cards to array
 function addToArray(){
 	openedCards.push(this);
 }
-
-//Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
 	var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -44,11 +63,9 @@ function shuffle(array) {
 	return array;
 }
 
-
-//Load shuffled deck at start of game
+/** Game initiation functions **/
 function startGame() {
 	openedCards=[];
-
 	let shuffledCards = shuffle(cards);
 	for (let i = 0; i < shuffledCards.length; i++) {
 		[].forEach.call(shuffledCards, function (item) {
@@ -56,12 +73,24 @@ function startGame() {
 		});
 		cards[i].classList.remove("show", "open", "match", "disabled", "noMatch");
 	}
-	moves = 0;
-	counter.innerHTML = moves;
+	move = 0;
+	counter.innerHTML = move;
+	for (let i = 0; i < stars.length; i++){
+		stars[i].style.visibility = "visible";
+	}
+	second = 0;
+	minute = 0;
+	hour = 0;
+	let clock = document.querySelector(".clock");
+	clock.innerHTML = "0 mins 0 secs";
+	clearInterval(interval);
+
 }
 window.onload = startGame();
 
-//Modal toggle functions
+
+
+/** Modal toggle functions **/
 function toggleModal() {
 	modal.classList.toggle("show-modal");
 }
@@ -70,12 +99,8 @@ function windowOnClick() {
 		toggleModal();
 	}
 }
-//Modal event listeners
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-modal.addEventListener("click", toggleModal);
 
-//check if opened cards match
+/** Matching functions **/
 function cardOpen() {
 	let length = openedCards.length;
 	if (length === 2) {
@@ -87,8 +112,6 @@ function cardOpen() {
 		}
 	}
 };
-
-//function when selections do match
 function matched() {
 	openedCards[0].classList.add("match", "disabled");
 	openedCards[1].classList.add("match", "disabled");
@@ -97,8 +120,6 @@ function matched() {
 	openedCards = [];
 	checkMatchedCards();
 }
-
-//function when selections do not match
 function unmatched() {
 	openedCards[0].classList.add("noMatch");
 	openedCards[1].classList.add("noMatch");
@@ -108,18 +129,21 @@ function unmatched() {
 		openedCards[1].classList.remove("show", "open", "noMatch");
 		enable();
 		openedCards = [];
-	}, 1100);
+	}, 0750);
+}
+function checkMatchedCards() {
+	if (matchedCard.length == 16) {
+		toggleModal();
+	}
 }
 
 
-//disable cards
+/** Disabling/enabling cards **/
 function disable() {
 	Array.prototype.filter.call(cards, function (card) {
 		card.classList.add('disabled');
 	});
 }
-
-//enable cards
 function enable() {
 	Array.prototype.filter.call(cards, function (card) {
 		card.classList.remove('disabled');
@@ -129,21 +153,34 @@ function enable() {
 	});
 }
 
-//Moves counter
+/** move count related functions **/
 function moveCounter() {
-	moves++;
-	counter.innerHTML = moves;
-}
-
-//Checks if all cards have been matched
-function checkMatchedCards(){
-	if (matchedCard.length == 16) {
-		toggleModal();
+	move++;
+	counter.innerHTML = move;
+	if (move > 8 && move < 12) {
+		for (i = 0; i < 3; i++) {
+			if (i > 1) {
+				stars[i].style.visibility = "collapse";
+			}
+		}
+	}
+	else if (move > 13) {
+		for (i = 0; i < 3; i++) {
+			if (i > 0) {
+				stars[i].style.visibility = "collapse";
+			}
+		}
+	}
+	if (move == 1) {
+		second = 0;
+		minute = 0;
+		hour = 0;
+		startTimer();
 	}
 }
 
-/*
 
+/*
 1. Display the cards on the page
 	A. DONE - shuffle the list of cards using the provided "shuffle" method.
 	B. DONE - loop through each card and create its HTML.
@@ -157,11 +194,11 @@ function checkMatchedCards(){
 		BB. DONE - if the cards don't match, remove them from the list and hide the card's symbol (put this functionality in another function)
 		CC. DONE - increment the move counter and display it on the page (put this functionality in another function)
 		DD.  - if all cards match, display a modal with a message, final rating, and time to win (put this functionality in another function)
-		EE.  - i n the modal, the player should be asked if they want to play again.
+		EE. DONE - in the modal, the player should be asked if they want to play again.
 
-3.  - When starting a game, a displayed timer should also start. Once the player wins the game, the timer stops.
+3. DONE - When starting a game, a displayed timer should also start. Once the player wins the game, the timer stops.
 
-4.  - The game should display a star rating (from 1 to 3) reflecting the player's performance. It should display at least 3 stars at the start and go down after some moves.
+4. DONE - The game should display a star rating (from 1 to 3) reflecting the player's performance. It should display at least 3 stars at the start and go down after some moves.
 
 5.  - A restart button should allow the player to reset the game board, the timer, and the star rating.
 
